e8456d60993251172c626ed3756602410f107d2b
LINUSOBURA <linusobura@gmail.com>
2024-08-02 01:20:34 +0300

    implemented hash_password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Encrypt Password"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using the bcrypt algorithm."""
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
