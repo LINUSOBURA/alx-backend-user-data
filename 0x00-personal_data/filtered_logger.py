7e612bb711c65231bf51ecd59d1934b589f3ee2c
LINUSOBURA <linusobura@gmail.com>
2024-08-02 00:40:41 +0300

    implemented get_db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -51,7 +51,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """Connects to a MySQL database using the credentials provided 
     in the environment variables"""
     mydb = mysql.connector.connect(
