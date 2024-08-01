de03f7de20ca8035b908135b001c5957c879cf46
LINUSOBURA <linusobura@gmail.com>
2024-08-01 22:13:40 +0300

    fixed length

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,5 @@ def filter_datum(fields, redaction, message, separator) -> str:
     """filter data to reduct"""
     pattern = r"(" + "|".join(
         [f"(?<={field}=)[^{separator}]+" for field in fields]) + r")"
-
     reducted = re.sub(pattern, redaction, message)
-
     return reducted
