628ace8dd57890ad0a4798ec437f8b2c607533ec
LINUSOBURA <linusobura@gmail.com>
2024-08-02 01:25:33 +0300

    implemented is_valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,11 @@ def hash_password(password: str) -> bytes:
     """Hashes a password using the bcrypt algorithm."""
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if a given password matches a hashed password."""
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    else:
+        return False
