5f0cd47c1f84cfe9c6081cfc88470d3a31f64558
LINUSOBURA <linusobura@gmail.com>
2024-08-06 20:51:27 +0300

    implemented authorization_header

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -26,7 +26,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Returns the authorization header for the given request."""
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        else:
+            return request.headers['Authorization']
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Returns the current user associated with the given request."""
