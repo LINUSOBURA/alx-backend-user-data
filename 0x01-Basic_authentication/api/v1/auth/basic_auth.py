9ab4104adab90e2f85c0e8be84ec1eec94dbb9d5
LINUSOBURA <linusobura@gmail.com>
2024-08-06 23:11:09 +0300

    implemented user_object_from_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,9 @@
 """Basic Auth Module"""
 
 import base64
+from typing import TypeVar
+
+from models.user import User
 
 from api.v1.auth.auth import Auth
 
@@ -45,3 +48,22 @@ class BasicAuth(Auth):
         else:
             email, password = decoded_base64_authorization_header.split(':', 1)
             return email, password
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns the User instance based on his email and password"""
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+        if user_email is None or user_pwd is None:
+            return None
+        try:
+            users = User.search({'email': user_email})
+            if not users:
+                return None
+            user = users[0]
+            if not user.is_valid_password(user_pwd):
+                return None
+            return user
+
+        except Exception:
+            return None
