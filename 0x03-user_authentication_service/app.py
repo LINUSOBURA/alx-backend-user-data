90af8d066951309880d21830278a1072c10413d9
LINUSOBURA <linusobura@gmail.com>
2024-08-14 21:46:39 +0300

    implemented logout function

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -43,7 +43,7 @@ def login() -> str:
 
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-def logout():
+def logout() -> None:
     """Logout a user"""
     session_id = request.cookies.get('session_id')
     if session_id is None:
